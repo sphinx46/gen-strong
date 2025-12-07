c267103c4de4fb4ced761887aa2bd1dc8e0615c2
Ilya Mordvinov <morilya111@gmail.com>
2025-12-07 11:02:20 +0300

    fix: исправление тестовых конфигураций .yaml

--- src/test/java/ru/cs/vsu/social_network/telegram_bot/TelegramBotApplicationTests.java
+++ src/test/java/ru/cs/vsu/social_network/telegram_bot/TelegramBotApplicationTests.java
@@ -3,13 +3,23 @@ package ru.cs.vsu.social_network.telegram_bot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = TelegramBotApplication.class
+)
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.main.allow-bean-definition-overriding=true",
+        "spring.autoconfigure.exclude=",
+        "telegram.bot.token=test_token",
+        "telegram.bot.username=test_bot",
+        "app.security.admin-telegram-ids=123456789"
+})
 class TelegramBotApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }

1f6c3007ca984cdd4d01388789af247e720afeaa
Ilya Mordvinov <morilya111@gmail.com>
2025-12-07 11:16:11 +0300

    chore: добавить недостающие test зависимости

--- src/test/java/ru/cs/vsu/social_network/telegram_bot/TelegramBotApplicationTests.java
+++ src/test/java/ru/cs/vsu/social_network/telegram_bot/TelegramBotApplicationTests.java
@@ -4,15 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = {
-                "spring.profiles.active=integration-test",
-                "spring.main.allow-bean-definition-overriding=true",
-                "spring.autoconfigure.exclude="
-        }
-)
-@ActiveProfiles("integration-test")
+@SpringBootTest
+@ActiveProfiles("test")
 class TelegramBotApplicationTests {
 
     @Test

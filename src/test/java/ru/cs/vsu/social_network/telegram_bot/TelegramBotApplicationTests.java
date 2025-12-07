ac998949b0f7c1897b73edce9028d06dc34533da
Ilya Mordvinov <morilya111@gmail.com>
2025-12-07 11:05:54 +0300

    fix: реализовать отдельную конфигурацию для интеграционных тестов

--- src/test/java/ru/cs/vsu/social_network/telegram_bot/TelegramBotApplicationTests.java
+++ src/test/java/ru/cs/vsu/social_network/telegram_bot/TelegramBotApplicationTests.java
@@ -3,20 +3,16 @@ package ru.cs.vsu.social_network.telegram_bot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-        classes = TelegramBotApplication.class
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "spring.profiles.active=integration-test",
+                "spring.main.allow-bean-definition-overriding=true",
+                "spring.autoconfigure.exclude="
+        }
 )
-@ActiveProfiles("test")
-@TestPropertySource(properties = {
-        "spring.main.allow-bean-definition-overriding=true",
-        "spring.autoconfigure.exclude=",
-        "telegram.bot.token=test_token",
-        "telegram.bot.username=test_bot",
-        "app.security.admin-telegram-ids=123456789"
-})
+@ActiveProfiles("integration-test")
 class TelegramBotApplicationTests {
 
     @Test
